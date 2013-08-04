01d840dfe24d64c1f1538c686cba1657c7c5d504
Blue Prawn <monnier.florent@gmail.com>
2013-08-04 13:30:52 +0200

    more convenient Rect.make

--- src/sdlrect.ml
+++ src/sdlrect.ml
@@ -1,10 +1,10 @@
 (* OCamlSDL2 - An OCaml interface to the SDL2 library
  Copyright (C) 2013 Florent Monnier
-
+ 
  This software is provided "AS-IS", without any express or implied warranty.
  In no event will the authors be held liable for any damages arising from
  the use of this software.
-
+ 
  Permission is granted to anyone to use this software for any purpose,
  including commercial applications, and to alter it and redistribute it freely.
 *)
@@ -18,8 +18,12 @@ type t = {
   h: int;
 }
 
-let make ~x ~y ~w ~h =
+let make ~pos:(x, y) ~dims:(w, h) =
+  { x; y; w; h }
+
+let make4 ~x ~y ~w ~h =
   { x; y; w; h }
 
 external has_intersection : a:t -> b:t -> bool
   = "caml_SDL_HasIntersection"
+
--- src/sdlrect.mli
+++ src/sdlrect.mli
@@ -1,10 +1,10 @@
 (* OCamlSDL2 - An OCaml interface to the SDL2 library
  Copyright (C) 2013 Florent Monnier
-
+ 
  This software is provided "AS-IS", without any express or implied warranty.
  In no event will the authors be held liable for any damages arising from
  the use of this software.
-
+ 
  Permission is granted to anyone to use this software for any purpose,
  including commercial applications, and to alter it and redistribute it freely.
 *)
@@ -20,7 +20,10 @@ type t = {
   h: int;
 }
 
-val make : x:int -> y:int -> w:int -> h:int -> t
+val make : pos:int * int -> dims:int * int -> t
+
+val make4 : x:int -> y:int -> w:int -> h:int -> t
 
 external has_intersection : a:t -> b:t -> bool
   = "caml_SDL_HasIntersection"
+
