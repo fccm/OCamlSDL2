e39760562623ab37a77d56334b5343b11f930e6d
Blue Prawn <monnier.florent@gmail.com>
2014-01-09 07:47:11 +0100

    relaying to the documentation for Read/Write Operations

--- src/sdlrwops.mli
+++ src/sdlrwops.mli
@@ -17,9 +17,11 @@ type t
 
 external from_mem : string -> t
   = "caml_SDL_RWFromMem"
+(** {{:http://wiki.libsdl.org/SDL_RWFromMem}api doc} *)
 
 external from_file : filename:string -> mode:string -> t
   = "caml_SDL_RWFromFile"
+(** {{:http://wiki.libsdl.org/SDL_RWFromFile}api doc} *)
 
 type input = [
   | `Filename of string   (** provide the input by its filename *)
@@ -43,23 +45,48 @@ type uint32 = int
 type uint64 = int
 
 external readU8 : t -> uint16 = "caml_SDL_ReadU8"
+(** {{:http://wiki.libsdl.org/SDL_ReadU8}api doc} *)
+
 external writeU8 : t -> uint16 -> int = "caml_SDL_WriteU8"
+(** {{:http://wiki.libsdl.org/SDL_WriteU8}api doc} *)
 
 module BigEndian : sig
   external read16 : t -> uint16 = "caml_SDL_ReadBE16"
+  (** {{:http://wiki.libsdl.org/SDL_ReadBE16}api doc} *)
+
   external read32 : t -> uint16 = "caml_SDL_ReadBE32"
+  (** {{:http://wiki.libsdl.org/SDL_ReadBE32}api doc} *)
+
   external read64 : t -> uint16 = "caml_SDL_ReadBE64"
+  (** {{:http://wiki.libsdl.org/SDL_ReadBE64}api doc} *)
+
   external write16 : t -> uint16 -> int = "caml_SDL_WriteBE16"
+  (** {{:http://wiki.libsdl.org/SDL_WriteBE16}api doc} *)
+
   external write32 : t -> uint16 -> int = "caml_SDL_WriteBE32"
+  (** {{:http://wiki.libsdl.org/SDL_WriteBE32}api doc} *)
+
   external write64 : t -> uint16 -> int = "caml_SDL_WriteBE64"
+  (** {{:http://wiki.libsdl.org/SDL_WriteBE64}api doc} *)
 end
 
 module LittleEndian : sig
   external read16 : t -> uint16 = "caml_SDL_ReadLE16"
+  (** {{:http://wiki.libsdl.org/SDL_ReadLE16}api doc} *)
+
   external read32 : t -> uint16 = "caml_SDL_ReadLE32"
+  (** {{:http://wiki.libsdl.org/SDL_ReadLE32}api doc} *)
+
   external read64 : t -> uint16 = "caml_SDL_ReadLE64"
+  (** {{:http://wiki.libsdl.org/SDL_ReadLE64}api doc} *)
+
   external write16 : t -> uint16 -> int = "caml_SDL_WriteLE16"
+  (** {{:http://wiki.libsdl.org/SDL_WriteLE16}api doc} *)
+
   external write32 : t -> uint16 -> int = "caml_SDL_WriteLE32"
+  (** {{:http://wiki.libsdl.org/SDL_WriteLE32}api doc} *)
+
   external write64 : t -> uint16 -> int = "caml_SDL_WriteLE64"
+  (** {{:http://wiki.libsdl.org/SDL_WriteLE64}api doc} *)
 end
 
