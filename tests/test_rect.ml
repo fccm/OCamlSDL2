4b17dd4c48aa277d79108a5dd038fe0a98560a8a
Florent Monnier <monnier.florent@gmail.com>
2019-03-13 04:06:00 +0059

    fixed minor things in test_rect.ml

--- tests/test_rect.ml
+++ tests/test_rect.ml
@@ -7,11 +7,11 @@ let check res should =
   else (err (); "Failed")
 
 let () =
-  let rect1 = Sdlrect.make 10 10 100  60 in
-  let rect2 = Sdlrect.make 20 20 200 120 in
-  let rect3 = Sdlrect.make 220 220 20 20 in
-  let r1, should1 = true,  Sdlrect.has_intersection ~a:rect1 ~b:rect2 in
-  let r2, should2 = false, Sdlrect.has_intersection ~a:rect1 ~b:rect3 in
+  let rect1 = Sdlrect.make4 10 10 100  60 in
+  let rect2 = Sdlrect.make4 20 20 200 120 in
+  let rect3 = Sdlrect.make4 220 220 20 20 in
+  let should1, r1 = true,  Sdlrect.has_intersection ~a:rect1 ~b:rect2 in
+  let should2, r2 = false, Sdlrect.has_intersection ~a:rect1 ~b:rect3 in
   Printf.printf "test 1: %s\n%!" (check r1 should1);
   Printf.printf "test 2: %s\n%!" (check r2 should2);
   exit !status
