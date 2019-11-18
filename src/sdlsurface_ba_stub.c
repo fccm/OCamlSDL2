28b9e89176e80256351e32dbc93e861f52a31c30
Florent Monnier <monnier.florent@gmail.com>
2019-11-18 17:50:37 +0059

    fixed Sdlsurface_ba.get_pixels, problem reported by Samuel Mimram <smimram@gmail.com> @smimram

--- src/sdlsurface_ba_stub.c
+++ src/sdlsurface_ba_stub.c
@@ -23,29 +23,11 @@ caml_SDL_Surface_ba_get_pixels(value surface)
 {
     SDL_Surface *surf = SDL_Surface_val(surface);
 
-    Uint8 bpp = surf->format->BitsPerPixel;
     int b_flag = 0;
     long dim = surf->h;
 
-    switch (bpp) {
-      case 8:
-        dim *= surf->pitch;
-        b_flag |= CAML_BA_UINT8; break;
-      case 16:
-        dim *= surf->pitch / 2;
-        b_flag |= CAML_BA_UINT16; break;
-      case 24:
-        dim *= surf->pitch;
-        b_flag |= CAML_BA_UINT8; break;
-      case 32:
-        dim *= surf->pitch / 4;
-        b_flag |= CAML_BA_INT32; break;
-      case 64:
-        dim *= surf->pitch / 8;
-        b_flag |= CAML_BA_INT64; break;
-      default:
-        caml_failwith("Sdlsurface_ba.get_pixels");
-    }
+    dim *= surf->pitch;
+    b_flag |= CAML_BA_UINT8;
 
     b_flag |= CAML_BA_C_LAYOUT | CAML_BA_EXTERNAL ;
 
