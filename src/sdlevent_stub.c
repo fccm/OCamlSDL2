0900fccfd0a1d31bed8e4bdd1a0f31b3ccea385c
Guillaume Munch-Maccagnoni <Guillaume.Munch-Maccagnoni@Inria.fr>
2020-09-29 11:01:26 +0200

    Naked pointers removal: add missing root

--- src/sdlevent_stub.c
+++ src/sdlevent_stub.c
@@ -24,10 +24,12 @@
 
 static value
 Val_some(value v)
-{   
-    value some = caml_alloc(1, 0);
+{
+    CAMLparam1(v);
+    CAMLlocal1(some);
+    some = caml_alloc(1, 0);
     Store_field(some, 0, v);
-    return some;
+    CAMLreturn(some);
 }
 
 #if 0
--- src/sdlrect_stub.c
+++ src/sdlrect_stub.c
@@ -23,9 +23,11 @@
 static value
 Val_some(value v)
 {
-    value some = caml_alloc(1, 0);
+    CAMLparam1(v);
+    CAMLlocal1(some);
+    some = caml_alloc(1, 0);
     Store_field(some, 0, v);
-    return some;
+    CAMLreturn(some);
 }
 
 CAMLprim value
